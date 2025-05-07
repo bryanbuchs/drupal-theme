680a4e4ca4e15b1d9fb56ab114c5f69ba16fbc6f
Bryan Buchs <bryan@kapowinc.com>
2025-05-07 14:39:50 -0500

    feat: add style dictionary configuration and color tokens
    - Updated package.json to include style-dictionary and new build-tokens script.
    - Added style-dictionary.config.js for managing design tokens.
    - Created colors.json to define color tokens.
    - Added themes.json to establish theme-based styling.
    - Generated variables.css with CSS variables for colors and themes.
    
    @TODO: integrate with storybook, vite

---
+++ style-dictionary.config.js
@@ -0,0 +1,27 @@
+export default {
+  "source": [
+    "tokens/definitions/*.json"
+  ],
+  "platforms": {
+    "css": {
+      "transformGroup": "css",
+      "buildPath": "tokens/",
+      "files": [
+        {
+          "destination": "css/variables.css",
+          "format": "css/variables"
+        }
+      ]
+    },
+    "less": {
+      "transformGroup": "less",
+      "buildPath": "less/settings/",
+      "files": [
+        {
+          "destination": "variables.less",
+          "format": "less/variables"
+        }
+      ]
+    }
+  }
+};
