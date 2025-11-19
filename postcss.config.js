01c3d67050c80c12472d3d8be42d00e2991c5e9c
Bryan Buchs <bryan@kapowinc.com>
2025-11-19 16:10:49 -0600

    feat: add postcss-utopia for responsive typography and update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 // import autoprefixer from 'autoprefixer'
 import inlineSvg from 'postcss-inline-svg'
 import postcssPresetEnv from 'postcss-preset-env'
+import postcssUtopia from 'postcss-utopia'
 
 export default {
   plugins: [
-
     inlineSvg({
       paths: [
         'node_modules/@fortawesome/fontawesome-free/svgs/',
@@ -13,5 +13,11 @@ export default {
       ]
     }),
     postcssPresetEnv(),
+    postcssUtopia({
+      minWidth: 428,
+      maxWidth: 1024,
+      minSize: 16,
+      maxSize: 18
+    })
   ]
 }
